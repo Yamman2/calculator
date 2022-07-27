b72470196bb87b21af586eaee53f8248bdb02e8a
Yamman2 <yammanrehan123@gmail.com>
2022-07-27 10:43:37 -0700

    todo complete

--- public/app.js
+++ public/app.js
@@ -49,7 +49,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
-function clearvalue() {
+/*function clearvalue() {
     document.getElementById("result").value = "";
 }
 
@@ -66,9 +66,87 @@ function calculate() {
     var p = document.getElementById("result").value;
     var q = eval(p);
     document.getElementById("result").value = q;
+}*/
+
+
+// var parent = document.getElementById("main")
+// var allPera = parent.getElementByTagName("p")
+// console.log(allPera);
+
+// for (let i = 0; i < allPera.length; i++) {
+//     allPera [i].className = "sucess";
+// }
+
+/*var parent = document.getElementById("inp")
+var textt = document.getElementById('main')
+
+function createElem(){
+    var newPera = document.createElement("P")
+    var newText = parent.value
+    newText = document.createTextNode(  newText)
+    newPera.appendChild(newText);
+
+    textt.appendChild(newPera)
+    console.log(parent);
+}*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+// todo
+
+var list = document.getElementById("list");
+
+function addtodo() {
+    var todo_item = document.getElementById("toodo-item");
+    
+    // create li tag with text node
+
+    var li = document.createElement("li");
+    var liText = document.createTextNode(todo_item.value)
+    li.setAttribute("class","lis")
+    li.appendChild(liText)
+    //   console.log(li)
+     
+    // input empty to next text to type 
+
+    todo_item.value =""
+    
+    // create button delete
+    
+    var delbtn = document.createElement("button")
+    var deltext = document.createTextNode("DELETE")
+    delbtn.setAttribute("class","btn")
+    delbtn.setAttribute("onclick","deleteItem(this)")
+    delbtn.appendChild(deltext)
+    
+    li.appendChild(delbtn)
+    list.appendChild(li)
+    
+    // create edite button
+    var edibtn = document.createElement("button") 
+    var ediText = document.createTextNode("EDITE")
+    edibtn.setAttribute("class","btn") 
+    edibtn.setAttribute("onclick","editeItem(this)") 
+    edibtn.appendChild(ediText)
+    
+    li.appendChild(edibtn)
+    
+
 }
 
+function editeItem(e){
+  var editvalue = prompt("Enter edit value", e.parentNode.firstChild.nodeValue)
+  e.parentNode.firstChild.nodeValue = editvalue
+    
+}
 
+function deleteItem(e){
+    
+    
+    e.parentNode.remove()
+}
 
-
+function deleteAll(){
+    
+    list.innerHTML = ""
+}
 
